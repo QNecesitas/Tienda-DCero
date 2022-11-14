884d8f44d1252164f432f16cd9d9d0e2ed671941
Marcos Suarez Garcia <marcossuarez01@gmail.com>
2022-11-14 17:53:57 +0100

    In Activity_ProductosEditar add first icons and Toolbar

---
+++ app/src/main/java/com/qnecesitas/tiendadcero_v10/Activity_EditarProductos.java
@@ -0,0 +1,14 @@
+package com.qnecesitas.tiendadcero_v10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_EditarProductos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_productos);
+    }
+}
