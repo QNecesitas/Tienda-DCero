07d77151a606931aed1722f3c988aed8799fe529
Ronny Nordelo Peña <102426420+ronnynor@users.noreply.github.com>
2022-11-18 01:37:44 -0500

    Update MainActivity.java

--- app/src/main/java/com/qnecesitas/tiendadcero_v10/MainActivity.java
+++ app/src/main/java/com/qnecesitas/tiendadcero_v10/MainActivity.java
@@ -11,7 +11,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("Hola mundo");
+        View escondedor=getWindow().getDecorView();
+        escondedor.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                |View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                |View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
+
+
+        //Handler
+        @SuppressLint("HandlerLeak") Handler handler=new Handler(){
+            @Override
+            public void handleMessage(Message message){
+                if(message.arg1==1){
+                    Intent intent=new Intent(MainActivity.this, Home.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+
+       
+        //Thread
+        Thread thread=new Thread(() ->{
+            try {
+                Thread.sleep(TIMEPO_DE_ESPERA);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+           Message message=Message.obtain();
+            message.arg1=1;
+            handler.sendMessage(message);
+        });
+        thread.start();
+
 
 
     }
